e42649a8bbe9b8e1196d31ee3dc1306b2f0a16b7
Dana <dana.brannon@utexas.edu>
2021-02-11 19:13:14 -0600

    Made book app and set up basic index view and route

--- myRecipes/urls.py
+++ myRecipes/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # path() is passed route and view (and kwargs and name are optional)
+    # the route is the end of the url (minus GET/POST params)
+    # django calls the specified view function with an HttpRequest obj and captured values from url as args
+    # name is so we can refer to it in templates in our project
+    path('book/', include('book.urls')), # include lets us reference the other url modules! use it for everything 
     path('admin/', admin.site.urls),
 ]
