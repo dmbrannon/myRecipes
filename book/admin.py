9ad88f210531d30d5f4f3ec9a2a5e38400d8221b
Dana <dana.brannon@utexas.edu>
2021-02-21 17:05:11 -0600

    Add recipe, ingredient, measurement to admin site

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Ingredient, Measurement
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Measurement)
