2faa0a948356aec5cabb1ca3cdf0b373b5c7f00d
Dana <dana.brannon@utexas.edu>
2021-02-24 22:23:45 -0600

    Add some basic views and an index template

--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /book/
+    path('', views.index, name='index'),
+    # ex: /book/5
+    path('<int:recipe_id>/', views.detail, name='detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Recipe
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the book index.")
-# Create your views here.
+    alpha_recipe_list = Recipe.objects.order_by('title')[:5]
+    # output = ', '.join([r.steps for r in alpha_recipe_list])
+    # return HttpResponse(output)
+    template = loader.get_template('book/index.html')
+    context = {
+        'alpha_recipe_list': alpha_recipe_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, recipe_id):
+    return HttpResponse(f"You're looking at recipe {recipe_id}.")
