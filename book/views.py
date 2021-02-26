0ea2901e94f190ba46fe4fbb688c65c6df3ca820
Dana <dana.brannon@utexas.edu>
2021-02-25 21:48:33 -0600

    Build out Detail view and use shortcuts

--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
 
 from .models import Recipe
 
@@ -8,11 +8,26 @@ def index(request):
     alpha_recipe_list = Recipe.objects.order_by('title')[:5]
     # output = ', '.join([r.steps for r in alpha_recipe_list])
     # return HttpResponse(output)
-    template = loader.get_template('book/index.html')
+    # template = loader.get_template('book/index.html')
     context = {
         'alpha_recipe_list': alpha_recipe_list,
     }
-    return HttpResponse(template.render(context, request))
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'book/index.html', context) # returns an HttpR with rendered template and context
 
 def detail(request, recipe_id):
-    return HttpResponse(f"You're looking at recipe {recipe_id}.")
+    '''try:
+        recipe = Recipe.objects.get(pk=recipe_id)
+    except Recipe.DoesNotExist:
+        raise Http404("Recipe does not exist.")'''
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    
+    # Steps looks like "Boil Water.  Set oven to 450.  Chop all vegetables."
+    steps = recipe.steps
+    steps_listed = steps.split("  ")
+    context = {
+        'recipe': recipe,
+        'steps': steps_listed,
+    }
+    
+    return render(request, 'book/detail.html', context)
