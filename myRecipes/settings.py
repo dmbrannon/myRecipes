8c02426b3770ba487ff26738b5975c7add04fe5b
Dana <dana.brannon@utexas.edu>
2021-04-12 20:50:58 -0500

    Update settings for Heroku

--- myRecipes/settings.py
+++ myRecipes/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dn#y+thp9vcbbt6xc$=_e8qnb(dw0t44=q*yregs1i!+()k4)r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dana-recipes.herokuapp.com']
 
 
 # Application definition
